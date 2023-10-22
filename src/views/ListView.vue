e341d5438ea5c9748f7923d1916c7ad6d61fd17c
kun99 <kuns.km@gmail.com>
2023-10-23 01:19:52 +0700

    fetching video

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,6 +1,28 @@
 <template>
-    <h1>My Videos</h1>
+  <h1 style="display: flex; justify-content: center">My Videos</h1>
+  <div v-for="video in videos" :key="video">
+    <video controls :src="video" width="400"></video>
+  </div>
 </template>
 
-<style>
-</style>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      videos: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:5001/videos")
+      .then((response) => {
+        this.videos = response.data.videos;
+      })
+      .catch((error) => {
+        console.error("Couldn't fetch videos:", error);
+      });
+  },
+};
+</script>
